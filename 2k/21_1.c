daca7d224f88010dc0217ca91b4b3707c1ba6382
NIKITA <sayonarabtch@gmail.com>
2018-04-14 00:10:27 +0300

    1

---
+++ 2k/21_1.c
@@ -0,0 +1,11 @@
+
+void push(struct node* head, int data) {
+struct node* newNode =(struct node*) malloc(sizeof(struct node));
+newNode->data = data;
+newNode->next = NULL;
+head = newNode;
+}
+void push_test() {
+List head = build();
+push(head, 1);
+}
